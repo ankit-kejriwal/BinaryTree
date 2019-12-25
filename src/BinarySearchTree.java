4d8a6b345f727f0f93b64ce66d99defd71e919f6
ankit <ankitkejriwaal@gmail.com>
2019-12-25 16:42:07 -0500

    Binary Search tree & iterative traversal

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class BinarySearchTree {
     Node root;
 
@@ -46,6 +50,58 @@ public class BinarySearchTree {
         System.out.print(root.key+ " ");
     }
 
+    public void preOrderIterative(Node root){
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            Node temp = stack.pop();
+            if(temp!=null){
+                System.out.print(temp.key+" ");
+            }
+            if(temp.right != null){
+                stack.push(temp.right);
+            }
+            if(temp.left != null){
+                stack.push(temp.left);
+            }
+        }
+    }
+
+    public void postOrderIterative(Node root){
+        Stack<Node> stack = new Stack<>();
+        List<Integer> list= new ArrayList<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            Node temp = stack.pop();
+            if(temp!=null){
+                list.add(0,temp.key);
+            }
+            if(temp.left != null){
+                stack.push(temp.left);
+            }
+            if(temp.right != null){
+                stack.push(temp.right);
+            }
+        }
+        for (int num:list) {
+            System.out.print(num+" ");
+        }
+    }
+
+    public void inOrderIterative(Node root){
+        Stack<Node> stack = new Stack<>();
+        Node temp = root;
+        while (!stack.isEmpty() || temp !=null){
+            while (temp != null){
+                stack.push(temp);
+                temp = temp.left;
+            }
+            temp = stack.pop();
+            System.out.print(temp.key +" ");
+            temp = temp.right;
+        }
+    }
+
     public static void main(String[] args){
         BinarySearchTree binarySearchTree = new BinarySearchTree();
         binarySearchTree.insert(11);
@@ -57,11 +113,23 @@ public class BinarySearchTree {
         binarySearchTree.insert(1);
         binarySearchTree.insert(90);
         binarySearchTree.insert(3);
+        System.out.println("PreOrder");
         binarySearchTree.preOrder(binarySearchTree.root);
         System.out.println();
+        System.out.println("PostOrder");
         binarySearchTree.postOrder(binarySearchTree.root);
         System.out.println();
+        System.out.println("InOrder");
         binarySearchTree.inOrder(binarySearchTree.root);
+        System.out.println();
+        System.out.println("PreOrder");
+        binarySearchTree.preOrderIterative(binarySearchTree.root);
+        System.out.println();
+        System.out.println("PostOrder");
+        binarySearchTree.postOrderIterative(binarySearchTree.root);
+        System.out.println();
+        System.out.println("InOrder");
+        binarySearchTree.inOrderIterative(binarySearchTree.root);
         System.out.println();
     }
 }
